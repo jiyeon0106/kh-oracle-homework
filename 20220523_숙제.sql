38219cfac43453a3f5bfe472dd83eae9e2c74c34
jiyeon0106 <jjjjj0820@naver.com>
2022-05-23 23:39:26 +0900

    0523숙제-순위

--- 20220523_숙제.sql
+++ 20220523_숙제.sql
@@ -4,6 +4,14 @@ SELECT workcd_vc, time_nu,
        RANK() OVER (ORDER BY time_nu DESC) AS TIME_RANK
 FROM t_worktime
 
+
+--내림차순 랭크
 SELECT workcd_vc, time_nu, 
        RANK() OVER (ORDER BY time_nu DESC) AS RNK
 FROM t_worktime
+
+
+--오름차순 랭크
+SELECT workcd_vc, time_nu, 
+       RANK() OVER (ORDER BY time_nu ) AS RNK
+FROM t_worktime 
